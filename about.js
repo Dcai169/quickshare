eca81fe1d438c447019ccef40ef97e6470607fb2
Daniel Cai <legoguy145@gmail.com>
2023-11-06 12:54:29 -0500

    add custom install flow

--- about.js
+++ about.js
@@ -1,16 +1,40 @@
 addEventListener("DOMContentLoaded", () => {
-    document.getElementById("installButton").addEventListener('click', async () => {
-        // deferredPrompt is a global variable we've been using in the sample to capture the `beforeinstallevent`
-        deferredPrompt.prompt();
-        // Find out whether the user confirmed the installation or not
-        const { outcome } = await deferredPrompt.userChoice;
-        // The deferredPrompt can only be used once.
-        deferredPrompt = null;
-        // Act on the user's choice
-        if (outcome === 'accepted') {
-          alert('User accepted the install prompt.');
-        } else if (outcome === 'dismissed') {
-            alert('User dismissed the install prompt');
+    let clientPlatform = navigator?.userAgentData.platform;
+    let installButton = document.getElementById("installButton");
+
+    window.addEventListener("DOMContentLoaded", async event => {
+        installButton.addEventListener("click", installApp);
+    });
+
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevents the default mini-infobar or install dialog from appearing on mobile
+        e.preventDefault();
+        // Save the event because you’ll need to trigger it later.
+        deferredPrompt = e;
+        // Show your customized install prompt for your PWA
+        if (clientPlatform == "Android" || clientPlatform == "ChromeOS") {
+            installButton.disabled = false;
         }
-      });
+    });
+
+    async function installApp() {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            // Find out whether the user confirmed the installation or not
+            const { outcome } = await deferredPrompt.userChoice;
+            // The deferredPrompt can only be used once.
+            deferredPrompt = null;
+            // Act on the user's choice
+            if (outcome === 'accepted') {
+                // showResult('😀 User accepted the install prompt.', true);
+            } else if (outcome === 'dismissed') {
+                // showResult('😟 User dismissed the install prompt');
+            }
+            // We hide the install button
+            document.querySelector("#install").style.display = "none";
+
+        }
+    }
 });
